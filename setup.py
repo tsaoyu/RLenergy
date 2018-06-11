80dfe7e5e52b63883fead3c9a6867be00823d846
Yu Cao <tsaoyu@tsaoyu.com>
2018-06-11 16:45:38 +0100

    Include setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='RLenergy',
+    version='0.0.1',
+    packages=['rlenergy','rlenergy_gym'],
+    license='MIT',
+    author='caoyu',
+    author_email='tsaoyu@gmail.com',
+    url='https://github.com/tsaoyu/RLenergy',
+    description='Reinforcement learning renewable energy'
+)
+
